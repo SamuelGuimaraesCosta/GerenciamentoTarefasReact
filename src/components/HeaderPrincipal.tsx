3e2a4bceef4fac9f73f039c85c4674aeecc5ceb9
Samuel Guimarães <samgcosta@hotmail.com>
2023-11-03 17:37:58 -0300

    creating a header in s-c for the main page

---
+++ src/components/HeaderPrincipal.tsx
@@ -0,0 +1,15 @@
+import { Header, HeaderTitle } from '../styled-components/Header';
+
+const HeaderPrincipal = () => {
+  return (
+    <div>
+      <Header>
+        <HeaderTitle>
+          TESTE
+        </HeaderTitle>
+      </Header>
+    </div>
+  );
+};
+
+export default HeaderPrincipal;
---
+++ src/styled-components/Header.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Header = styled.header`
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+`;
+
+const HeaderTitle = styled.h1`
+  font-size: 2rem;
+`;
+
+const HeaderSubtitle = styled.p`
+  font-size: 1rem;
+`;
+
+export { Header, HeaderTitle, HeaderSubtitle };
