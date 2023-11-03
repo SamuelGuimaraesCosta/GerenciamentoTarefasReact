b79032d6381235a8fb83d0373e7287ad8c05f4a3
Samuel Guimarães <samgcosta@hotmail.com>
2023-11-03 18:00:47 -0300

    adding router to project and MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import HeaderPrincipal from './components/HeaderPrincipal';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
-    <HeaderPrincipal />
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderPrincipal from '../components/HeaderPrincipal';
+
+function HomePage() {
+  return (
+    <div>
+      <HeaderPrincipal />
+    </div>
+  );
+}
+
+export default HomePage;
