a48dce5c0d669e5b13afe51d6b2f5d42acdbdfc0
Samuel Guimarães <samgcosta@hotmail.com>
2023-11-05 14:44:17 -0300

    header css upgrades

--- src/components/HeaderPrincipal.tsx
+++ src/components/HeaderPrincipal.tsx
@@ -1,12 +1,37 @@
-import { Header, HeaderTitle } from '../styled-components/Header';
+import { Header, HeaderContent, HeaderTitle, Logo, Navbar, TaskCounter, NewTaskButton } from '../styled-components/Header';
+import { useState, useEffect } from 'react';
 
 const HeaderPrincipal = () => {
+  const [taskCount, setTaskCount] = useState(0);
+  const [headerBackgroundColor, setHeaderBackgroundColor] = useState('blue');
+
+  const addTask = () => {
+    setTaskCount(taskCount + 1);
+  };
+
+  useEffect(() => {
+    if (taskCount >= 1 && taskCount <= 3) {
+      setHeaderBackgroundColor('green');
+    } else if (taskCount >= 10) {
+      setHeaderBackgroundColor('red');
+    } else {
+      setHeaderBackgroundColor('blue');
+    }
+  }, [taskCount]);
+
   return (
     <div>
-      <Header>
-        <HeaderTitle>
-          TESTE
-        </HeaderTitle>
+      <Header bgColor={headerBackgroundColor}>
+        <HeaderContent>
+          <Logo src='task-logo.png'></Logo>
+          <HeaderTitle>
+            Gerenciamento de Tarefas
+          </HeaderTitle>
+        </HeaderContent>
+        <Navbar>
+          <TaskCounter bgColor={headerBackgroundColor}>Tarefas Pendentes: {taskCount}</TaskCounter>
+          <NewTaskButton onClick={addTask}>Nova Tarefa</NewTaskButton>
+        </Navbar>
       </Header>
     </div>
   );
--- src/styled-components/Header.tsx
+++ src/styled-components/Header.tsx
@@ -1,18 +1,109 @@
 import styled from 'styled-components';
 
-const Header = styled.header`
-  background-color: #333;
+interface HeaderProps {
+  bgColor: string;
+}
+
+const Header = styled.header<HeaderProps>`
+  background-color: ${({ bgColor }) => bgColor};
   color: #fff;
   padding: 20px;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-height: 100px !important;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    text-align: center;
+  }
+`;
+
+const Logo = styled.img`
+  max-width: 100px;
+  margin-right: 20px;
+  @media (max-width: 768px) {
+    margin-right: 0;
+    margin-bottom: 10px;
+  }
+`;
+
+const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const HeaderTitle = styled.h1`
-  font-size: 2rem;
+  font-size: 1.6rem;
 `;
 
 const HeaderSubtitle = styled.p`
   font-size: 1rem;
+  color: #0074e4; /* Cor azul padrão */
+
+  @media (max-width: 768px) {
+    color: #ff0000; /* Cor vermelha na visualização móvel */
+  }
 `;
 
-export { Header, HeaderTitle, HeaderSubtitle };
+const Navbar = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const TaskCounter = styled.div<HeaderProps>`
+  margin-right: 20px;
+  font-weight: bold;
+  font-size: 1.2rem;
+  animation: pulse 3.5s infinite; /* Adicione a animação de pulso */
+
+  @media (max-width: 768px) {
+    margin: 10px 0;
+    color: ${({ bgColor }) => bgColor};
+  }
+
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+      opacity: 1;
+    }
+    50% {
+      transform: scale(1.1);
+      opacity: 0.8;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
+`;
+
+const NewTaskButton = styled.button`
+  background-color: #0074e4;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  transition: background-color 0.2s;
+  
+  &:hover {
+    background-color: #005bab;
+  }
+
+  &:active { /* Estilize a animação quando o botão estiver ativo (clicado) */
+    transform: scale(0.95); /* Reduza o tamanho em 5% quando clicado */
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export { Header, Logo, HeaderTitle, HeaderSubtitle, Navbar, HeaderContent, TaskCounter, NewTaskButton };
